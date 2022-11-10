1e5991c3c09b028f1b2a291d0e614fd01e917389
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-11-10 07:14:06 +0600

    my reviews added

---
+++ src/Pages/MyReviews/MyReviews.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+
+const MyReviews = () => {
+    const { user } = useContext(AuthContext);
+    const [reviews, setReviews] = useState({})
+
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email])
+
+    return (
+        <div>
+            <h1>my reviews{reviews.length}</h1>
+        </div>
+    );
+};
+
+export default MyReviews;
