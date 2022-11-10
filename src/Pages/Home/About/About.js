e09b07e0c415de842aa76430dc872ee4b1c6f01f
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-11-10 22:17:31 +0600

    last commit

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import img from '../../../assets/images/banner/photographersrightsUS.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h1 className='text-center text-3xl'>What Say's our client</h1>
+        <div className="hero mt-20 ">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={img} className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Photography make us happy take a shot</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                </div>
+            </div>
         </div>
     );
 };
